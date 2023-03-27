3144eaee0a9348632b3db1ed85faef8f396fe724
Eunji Kim <47842737+eunjiiiiii@users.noreply.github.com>
2023-03-27 15:40:07 +0900

    Part11_김은지

--- 김은지/Part11_해시테이블/[프로그래머스] 위장.py
+++ 김은지/Part11_해시테이블/[프로그래머스] 위장.py
@@ -1,3 +1,23 @@
+"""
+이전 코드
+import collections
+
+def solution(clothes):
+    answer = 0
+    
+    dict_clothes = collections.defaultdict(list)
+    
+    for c, category in clothes:
+        dict_clothes[category].append(c)
+    
+    res = 1
+    for v in dict_clothes.values():
+        res *= (len(v) + 1)
+    return res - 1
+
+"""
+
+
 import collections
 
 def solution(clothes):
